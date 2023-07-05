000a58e76a52b44221512120a09fefe9f6d06f07
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-05 19:48:30 +0300

    ServiceContext setup

---
+++ src/context/ServiceContext.js
@@ -0,0 +1,11 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+export const ServiceContext = createContext();
+
+const ServiceContext = (props) => {
+
+
+}
+
+
+export default ServiceContext;
