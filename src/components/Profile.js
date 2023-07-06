38f7b5ac4dc27315d3d84caa5b4c2620c4e80783
Yazeed <dee3ayahmed25@gmail.com>
2023-07-06 15:17:57 +0300

    fetching customer profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,5 +5,24 @@ import { Button, Input } from "@chakra-ui/react";
 
 const profile = () => {
     const [customerProfile, setCustomerProfile] = useState({});
-   const [authentication, setAuthentication] = useState(true); 
+   const [authentication, setAuthentication] = useState(true);
 }
+
+useEffect(() => {
+  const fetchCustomerProfile = async () => {
+    try {
+      const response = await fetch("/api/customers/:customerId"); // Replace ":customerId" with the actual ID of the logged-in customer
+      if (response.ok) {
+        const profileData = await response.json();
+        setCustomerProfile(profileData);
+      } else {
+        const errorData = await response.json();
+        console.error("Failed to fetch customer profile:", errorData);
+      }
+    } catch (error) {
+      console.error("An error occurred while fetching customer profile:", error);
+    }
+  };
+
+  fetchCustomerProfile();
+}, []);
