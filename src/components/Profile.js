1aa5743d0e96f092c8616be210fcb7098a32e4bc
Yazeed <dee3ayahmed25@gmail.com>
2023-07-06 12:33:40 +0300

    import dependencies formik,yup and chakra UI

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,1 +1,4 @@
 import React, { useEffect, useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { Button, Input } from "@chakra-ui/react";
