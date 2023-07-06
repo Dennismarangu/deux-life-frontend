2f5f5dd74af7577b5ab9b9992c1bc2d5b3781ccc
Alex Njagi <1992anjagi@gmail.com>
2023-07-06 13:56:15 +0300

    Creating navbar somewhat successful

--- src/App.js
+++ src/App.js
@@ -1,53 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { ChakraProvider, Box, Heading, Container, VStack } from '@chakra-ui/react';
-import BookingForm from './components/BookingForm';
+import * as React from 'react'
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
-  const [rooms, setRooms] = useState([]);
-  const [selectedRoom, setSelectedRoom] = useState(null);
-
-  const handleRoomClick = (room) => {
-    setSelectedRoom(room);
-  };
-
-  // Fetch rooms data from the backend using an API request
-  useEffect(() => {
-    fetch('http://localhost:3000/rooms')
-      .then(response => response.json())
-      .then(data => setRooms(data))
-      .catch(error => console.error('Error fetching rooms:', error));
-  }, []);
-
+  // 2. Wrap ChakraProvider at the root of your app
   return (
     <ChakraProvider>
-      <Box bg="gray.100" minH="100vh" py={8}>
-        <Container maxW="xl">
-          <VStack spacing={6}>
-            <Heading as="h1" size="xl">Room List</Heading>
-            {rooms.map((room) => (
-              <Box
-                key={room.id}
-                onClick={() => handleRoomClick(room)}
-                className="room-item"
-                p={4}
-                boxShadow="md"
-                borderRadius="md"
-                cursor="pointer"
-              >
-                <Heading as="h3" size="md" mb={2}>
-                  {room.room_name}
-                </Heading>
-                <Box>{room.room_description}</Box>
-                <Box>Capacity: {room.room_capacity}</Box>
-                <Box>Price: {room.room_price}</Box>
-              </Box>
-            ))}
-            {selectedRoom && <BookingForm room={selectedRoom} />}
-          </VStack>
-        </Container>
-      </Box>
+    
     </ChakraProvider>
-  );
+  )
 }
 
 export default App;
