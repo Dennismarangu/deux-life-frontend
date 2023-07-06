d6b809fbbb1c26ad4bc594e43c28f1890d69c3d2
Yazeed <dee3ayahmed25@gmail.com>
2023-07-06 15:25:12 +0300

    validationSchema defined using Yup's

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -26,3 +26,14 @@ useEffect(() => {
 
   fetchCustomerProfile();
 }, []);
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string().email("Invalid email address").required("Email is required"),
+    password: Yup.string().required("Password is required").min(6, "Password must be at least 6 characters"),
+    password_confirmation: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Password confirmation is required"),
+  });
+
+
