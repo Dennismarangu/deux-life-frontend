c5f5a4898f45f84fb477e255ed6e23b4a4254d8a
Yazeed <dee3ayahmed25@gmail.com>
2023-07-06 12:38:00 +0300

    set customerprofile to update its value

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,3 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { Button, Input } from "@chakra-ui/react";
+
+const profile = () => {
+    const [customerProfile, setCustomerProfile] = useState({});
+  const [authentication, setAuthentication] = useState(true);
+}
