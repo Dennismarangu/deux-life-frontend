fd54d7e97a9fc8761adbc823e3b3b81ad90dc8df
Alex Njagi <1992anjagi@gmail.com>
2023-07-06 15:13:12 +0300

    Backgroundimage rendering successfully

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import { NavBar } from './components/NavBar';
+import { Header } from './components/Header';
 
 function App() {
   const [page, setPage] = useState("/");
   return (
     <>
+      <Box>
+        <Flex position="fixed" top={0} left={0} right={0} justifyContent="center" zIndex="999">
+          <nav>
+            <Header />
+          </nav>
+        </Flex>
+      </Box>        
+
       <Routes>
         <Route path="/" element={
           <Box
-            bgImage={"url('/src/images/background.jpg')"}
+            bgImage={require('./images/background.jpg')}
             bgSize="cover"
             bgPosition="center"
-            minHeight="100vh"
             display="flex"
             justifyContent="center"
             alignItems="center"
-          >
-            <Box width="300px" height="200px" display="flex" flexDirection="column" justifyContent="center">
-              <Box alignSelf="flex-start" ml={4}>
-                <nav>
-                  <NavBar onChangePage={setPage} />
-                </nav>
-              </Box>
-            </Box>
+            minHeight="100vh"
+            // style={{
+            //   backgroundImage: '/images/background.jpg'
+            // }}
+        >
+          <Box width="300px" height="200px" display="flex" flexDirection="column" justifyContent="center" textAlign="center">
+            <nav>
+              <NavBar onChangePage={setPage} />
+            </nav>
           </Box>
+        </Box>
         }/>
       </Routes>
     </>
