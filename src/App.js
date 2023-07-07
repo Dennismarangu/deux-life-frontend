f2d07cb33f84f26c5db9f01e9e026f39d54b3f90
Alex Njagi <1992anjagi@gmail.com>
2023-07-07 11:15:34 +0300

    Homepage completed

--- src/App.js
+++ src/App.js
@@ -3,37 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import { Homepage } from './components/Homepage';
 
 function App() {
+
   return (
     <>
-      <Box>
-        <Flex position="fixed" top={0} left={0} right={0} justifyContent="center" zIndex="999">
-          <nav>
-            <Header />
-          </nav>
-        </Flex>
-      </Box>        
-
       <Routes>
-        <Route path="/" element={
-          <Box
-            bgImage={require('./images/background.jpg')}
-            bgSize="cover"
-            bgPosition="center"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            minHeight="100vh"
-            // style={{
-            //   backgroundImage: '/images/background.jpg'
-            // }}
-        >
-          <Box width="300px" height="200px" display="flex" flexDirection="column" justifyContent="center" textAlign="center">
-            <nav>
-              <NavBar onChangePage={setPage} />
-            </nav>
-          </Box>
-        </Box>
-        }/>
+        <Route path="/" element={<Homepage />}/>
       </Routes>
     </>
   );
