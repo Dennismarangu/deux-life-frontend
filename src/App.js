f5b2e43ad19d49d3ac732ae1c9234f6f27935f3e
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-08 19:52:07 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Homepage } from './components/Homepage';
+import { Box, Container, VStack, Heading } from '@chakra-ui/react';
+import { BookingForm } from './components/BookingForm';
 
 import Service from './components/Service';
 import ServiceContextProvider from './context/ServiceContext';
 
+
+
 function App() {
+  const [selectedRoom, setSelectedRoom] = React.useState(null);
+
+  const rooms = [
+    { id: 1, room_name: 'Room 1', room_description: 'Description 1', room_capacity: 2, room_price: 100 },
+    { id: 2, room_name: 'Room 2', room_description: 'Description 2', room_capacity: 4, room_price: 200 },
+    // Add more rooms as needed
+  ];
+
+  const handleRoomClick = (room) => {
+    // Implement the logic for handling room click
+  };
+
+
   return (
-    <>   
+    <>
       <Routes>
         <Route path="/" element={<Homepage />}/>
         <Route path="/service" element={
           <ServiceContextProvider>
              <Service />
         </ServiceContextProvider>} />
-        
+
 
         <Route path="/book" element={<Box bg="gray.100" minH="100vh" py={8}>
         <Container maxW="xl">
